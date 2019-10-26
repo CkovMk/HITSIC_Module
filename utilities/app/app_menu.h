b7efee35a63f414a9466b840eeae1a6c98344539
Chekhov Mark <maqikewangyi@163.com>
2019-10-26 08:36:58 +0800

    menu update

--- utilities/app/app_menu.h
+++ utilities/app/app_menu.h
@@ -260,42 +260,43 @@ struct menu_itemIfce_t
  */
 
 /**
- * @brief : 
+ * @brief : 菜单项结构体的构造函数。
+ * 该函数分配一块内存，根据传入的参数填入内容，然后返回所产生的菜单项结构体指针。
  * 
- * @param  {menu_itemType_t} _type : 
- * @param  {void*} _data           : 
- * @param  {char*} _nameStr        : 
- * @param  {uint32_t} _saveAddr    : 
- * @param  {uint32_t} _pptFlag     : 
+ * @param  {menu_itemType_t} _type : 要生成的菜单项类型。。
+ * @param  {void*} _data           : 指向要操作的数据的指针。
+ * @param  {char*} _nameStr        : 菜单项名称字符串，不超过最大长度，以'\0'结尾。
+ * @param  {uint32_t} _saveAddr    : 变量要保存到的地址。对非数据类型及属性设置中只读的菜单项无效。
+ * @param  {uint32_t} _pptFlag     : 属性标志。
  * 
- * @return {menu_itemIfce_t *}     :
+ * @return {menu_itemIfce_t *}     : 返回所产生的菜单项结构体指针。
  */
 menu_itemIfce_t *MENU_itemConstruct(menu_itemType_t _type, void *_data, const char *_nameStr, uint32_t _saveAddr, uint32_t _pptFlag);
 
 
 /**
- * @brief : 
+ * @brief : 菜单项结构体析构函数。
  * 
- * @param  {menu_itemIfce_t*} _item : 
+ * @param  {menu_itemIfce_t*} _item : 要析构的菜单项结构体指针。析构后该指针会被自动赋值为NULL。
  */
 void MENU_itemDestruct(menu_itemIfce_t *_item);
 
 
 
 /**
- * @brief : 
+ * @brief : 获取菜单项内存放的数据指针。
  * 
- * @param  {menu_itemIfce_t*} _item : 
+ * @param  {menu_itemIfce_t*} _item : 要访问的菜单项的指针
  * 
- * @return {void *}					:
+ * @return {void *}					: 返回数据指针。注意，无论何种类型，都会返回对应的数据指针。
  */
 void *MENU_ItemGetData(menu_itemIfce_t *_item);
 
 
 /**
- * @brief : 
- * 
- * @param  {menu_itemIfce_t*} _item : 
+ * @brief : 设置菜单项内指针指向的数据的值。
+ * 注意，该函数对非数据类型菜单和只读数据菜单无效。
+ * @param  {menu_itemIfce_t*} _item : 要访问的菜单项的指针
  * @param  {void*} _data            : 
  */
 void MENU_ItemSetData(menu_itemIfce_t *_item, void *_data);
@@ -305,19 +306,19 @@ void MENU_ItemSetData(menu_itemIfce_t *_item, void *_data);
 
 
 /**
- * @brief : 
+ * @brief : 在菜单列表中打印一行菜单。
  * 
- * @param  {menu_itemIfce_t*} _item : 
- * @param  {uint32_t} _slotNum      : 
+ * @param  {menu_itemIfce_t*} _item : 要访问的菜单项的指针。
+ * @param  {uint32_t} _slotNum      : 要打印位置（第几行）。对于12864 OLED而言，取值范围为0~7。
  */
 void MENU_ItemPrintSlot(menu_itemIfce_t *_item, uint32_t _slotNum);
 
 
 /**
- * @brief : 
+ * @brief : 在菜单列表中响应直接按键操作。
  * 
- * @param  {menu_itemIfce_t*} _item : 
- * @param  {menu_keyOp_t*} _op      : 
+ * @param  {menu_itemIfce_t*} _item : 要访问的菜单项的指针。
+ * @param  {menu_keyOp_t*} _op      : 按键操作指针。按键响应后会被清除为空操作。
  */
 void MENU_ItemDirectKeyOp(menu_itemIfce_t *_item, menu_keyOp_t *_op);
 
@@ -325,17 +326,17 @@ void MENU_ItemDirectKeyOp(menu_itemIfce_t *_item, menu_keyOp_t *_op);
 //used when in menuItem
 
 /**
- * @brief : 
+ * @brief : 打印访问菜单项的二级页面。
  * 
- * @param  {menu_itemIfce_t*} _item : 
+ * @param  {menu_itemIfce_t*} _item : 要访问的菜单项的指针。
  */
 void MENU_ItemPrintDisp(menu_itemIfce_t *_item);
 
 /**
- * @brief : 
+ * @brief : 访问菜单项的二级页面时响应按键操作。
  * 
- * @param  {menu_itemIfce_t*} _item : 
- * @param  {menu_keyOp_t*} _op      : 
+ * @param  {menu_itemIfce_t*} _item : 要访问的菜单项的指针。
+ * @param  {menu_keyOp_t*} _op      : 按键操作指针。按键响应后会被清除为空操作。
  */
 void MENU_ItemKeyOp(menu_itemIfce_t *_item, menu_keyOp_t *_op);
 
@@ -344,56 +345,59 @@ void MENU_ItemKeyOp(menu_itemIfce_t *_item, menu_keyOp_t *_op);
 
 
 
-
+/**
+ * 菜单列表结构体。
+ */
 struct menu_list_t
 {
-	menu_itemIfce_t *menu;
-	uint32_t listSize, listNum;
-	uint32_t disp_p, slct_p;
-	char nameStr[menu_nameStrSize];
-	menu_list_t *prev;
+	menu_itemIfce_t *menu;				/// 菜单项指针的动态数组，用于存放指针。该数组内的指针析构时需要手动释放。
+	uint32_t listSize, listNum;			/// 当前菜单项指针列表的大小、当前列表内的菜单项数量。
+	uint32_t disp_p, slct_p;			/// 显示数组下标和选择数组下标。
+	char nameStr[menu_nameStrSize];		/// 菜单列表名称字符串。
+	menu_list_t *prev;					/// 指向上级菜单的指针。根菜单中该指针指向自身。
 };
 
-
 /**
- * @brief : 
+ * @brief : 菜单列表的构造函数。
  * 
- * @param  {menu_list_t*} _list   : 
  * @param  {const char*} _nameStr : 
  * @param  {uint32_t} _listSize   : 
  * @param  {menu_list_t*} _prev   : 
+ * 
+ * @return {menu_list_t *}        : 返回构造的菜单列表结构体指针。
  */
-void MENU_ListConstruct(menu_list_t *_list, const char *_nameStr, uint32_t _listSize , menu_list_t* _prev);
+menu_list_t *MENU_ListConstruct(const char *_nameStr, uint32_t _listSize , menu_list_t* _prev);
 
 /**
- * @brief : 
+ * @brief : 菜单列表的析构函数。
  * 
- * @param  {menu_list_t*} _list : 
+ * @param  {menu_list_t*} _list : 要析构的菜单列表结构体指针。析构后该指针会被自动赋为NULL。
  */
 void MENU_ListDestruct(menu_list_t *_list);
 
 /**
+ * @brief : 插入一个菜单项。
  * 
- * @param  {menu_list_t*} _list     : 
- * @param  {menu_itemIfce_t*} _item : 
- * @return {status_t}               : 
+ * @param  {menu_list_t*} _list     : 要访问的菜单列表的指针。
+ * @param  {menu_itemIfce_t*} _item : 要插入的菜单项指针。该指针应为将亡值。
+ * @return {status_t}               : 返回操作的结果。正常应返回kStatus_Success。
  */
 status_t MENU_ListInsert(menu_list_t *_list, menu_itemIfce_t *_item);
 
 /**
- * @brief : 
+ * @brief : 打印菜单列表。
  * 
- * @param  {menu_list_t*} _list : 
+ * @param  {menu_list_t*} _list : 要访问的菜单列表的指针。
  */
 void MENU_ListPrintDisp(menu_list_t *_list);
 
 /**
- * @brief : 
+ * @brief : 菜单列表中响应按键操作。
  * 
- * @param  {menu_list_t*} _list    : 
- * @param  {appVar_keyBTOp_t*} _op : 
+ * @param  {menu_list_t*} _list : 要访问的菜单列表的指针。
+ * @param  {menu_keyOp_t*} _op  : 按键操作指针。按键响应后会被清除为空操作。
  */
-void MENU_ListKeyOp(menu_list_t *_list, appVar_keyBTOp_t* _op);
+void MENU_ListKeyOp(menu_list_t *_list, menu_keyOp_t* _op);
 
 
 /**
@@ -404,68 +408,91 @@ void MENU_ListKeyOp(menu_list_t *_list, appVar_keyBTOp_t* _op);
 //rg = region
 //addr = address
 //sect = sector
+/**
+ * @brief : 每个扇区包含的字节数
+ */
 uint32_t menu_nvm_sectorSize = 4096u;
-//global storage
-uint32_t menu_nvm_glSectCnt = 2u;
-uint32_t menu_nvm_glSectOffset = 2u; 
-uint32_t menu_nvm_glAddrOffset = menu_nvm_glSectOffset * menu_nvm_sectorSize;
-//region storage
-uint32_t menu_nvm_rgCnt = 3u, menu_nvm_rgSectCnt = 4u;
-uint32_t menu_nvm_rgSectOffset[menu_nvm_rgCnt] = 
+/**
+ * @brief : 全局存储 Global Storage
+ */
+uint32_t menu_nvm_glSectCnt = 2u;					/// 全局存储区占用的扇区数
+uint32_t menu_nvm_glSectOffset = 2u; 				/// 全局存储区扇区偏移
+uint32_t menu_nvm_glAddrOffset = menu_nvm_glSectOffset * menu_nvm_sectorSize;	/// 全局存储区地址偏移
+/**
+ * @brief : 局部存储 Region Storage
+ */
+uint32_t menu_nvm_rgCnt = 3u						/// 局部存储区的数量
+uint32_t menu_nvm_rgSectCnt = 4u;					/// 每个局部存储区占用的扇区数
+uint32_t menu_nvm_rgSectOffset[menu_nvm_rgCnt] = 	/// 三个局部存储区的扇区偏移
 {
 	menu_nvm_glSectOffset + menu_nvm_glSectCnt + 0u * menu_nvm_rgSectCnt,
 	menu_nvm_glSectOffset + menu_nvm_glSectCnt + 1u * menu_nvm_rgSectCnt,
 	menu_nvm_glSectOffset + menu_nvm_glSectCnt + 2u * menu_nvm_rgSectCnt,
 };
-uint32_t menu_nvm_rgAddrOffset[menu_nvm_rgCnt] = 
+uint32_t menu_nvm_rgAddrOffset[menu_nvm_rgCnt] = 	/// 三个局部存储区的地址偏移
 {
 	menu_nvm_rgSectOffset[0] * menu_nvm_sectorSize,
 	menu_nvm_rgSectOffset[1] * menu_nvm_sectorSize,
 	menu_nvm_rgSectOffset[2] * menu_nvm_sectorSize,
 };
-
-uint32_t menu_nvm_totalSectorCnt = menu_nvm_globalSectorCnt + menu_nvm_regionCnt * menu_nvm_regionSectorCnt;
-
+/**
+ * @brief : 菜单存储占用的总扇区数
+ */
+uint32_t menu_nvm_totalSectCnt = menu_nvm_globalSectorCnt + menu_nvm_regionCnt * menu_nvm_regionSectorCnt;
+/**
+ * @brief : 每个菜单项保存时占用的字节数
+ */
 uint32_t menu_nvm_dataSize = 32u;
 
+/**
+ * @brief : 菜单项写入缓存。
+ * 当改写第N个扇区时，menu_nvm_cachedSector = N, menu_nvm_cache分配4KB缓存
+ * 并读入第N扇区的所有内容。此时能且仅能修改第N扇区的内容。对第N扇区内容的修改
+ * 将缓存至上述内存。
+ */
 uint8_t *menu_nvm_cache;
 uint32_t menu_nvm_cachedSector;
 
 
 
 /**
- * @brief : 
+ * @brief : 从NVM中读取数据。
  * 
- * @param  {uint32_t} _addr    : 
- * @param  {void*} _buf        : 
- * @param  {uint32_t} _byteCnt : 
- * @return {status_t}          : 
+ * @param  {uint32_t} _addr    : 要读取的NVM地址。
+ * @param  {void*} _buf        : 用于保存数据的缓冲区。
+ * @param  {uint32_t} _byteCnt : 要读取的字节数。
+ * 
+ * @return {status_t}          : 返回运行结果。正常返回kStatus_Success。
  */
 status_t MENU_NvmRead(uint32_t _addr, void *_buf, uint32_t _byteCnt);
 
 /**
- * @brief : 
+ * @brief : 检查地址是否在可缓存范围内。
+ * 如果当前未缓存任何扇区（menu_nvm_cache == NULL），始终返回true。如果当前已有缓存扇区，则根据保存的
+ * 扇区号判断输入地址是否在缓存范围内。
  * 
- * @param  {uint32_t} _addr : 
- * @return {bool}           : 
+ * @param  {uint32_t} _addr : 要检查的地址
+ * @return {bool}           : 可缓存返回true，不可缓存返回false。
  */
 bool MENU_NvmCacheable(uint32_t _addr);	//check if addr is ready to write.
 
 /**
- * @brief : 
- * 
+ * @brief : 将数据写入缓存。
+ * 该函数会检查输入地址及数据大小是否在可缓存范围内。
+ * 如果当前未缓存任何扇区，将创建缓存。
  * @param  {uint32_t} _addr    : 
  * @param  {void*} _buf        : 
  * @param  {uint32_t} _byteCnt : 
- * @return {status_t}          : 
+ * @return {status_t}          : 成功写入返回kStatus_Success，失败返回kStatus_Fail。
  */
 status_t MENU_NvmWriteCache(uint32_t _addr, void *_buf, uint32_t _byteCnt);
 
 /**
- * @brief : 
+ * @brief : 将缓存的修改写回NVM。
+ * 该函数先擦书缓存的扇区，然后将缓存的内容写回该扇区，最后释放缓存区内存，将缓存区指针赋为NULL。
+ * 缓存的扇区号将保留原值。
  * 
- * @param  {void} undefined : 
- * @return {status_t}       : 
+ * @return {status_t}       : 成功写回返回kStatus_Success。
  */
 status_t MENU_NvmUpdateCache(void);
 
@@ -526,67 +553,72 @@ menu_t menuInst;
 
 
 /**
- * @brief : 
+ * @brief : 菜单全局初始化。
+ * 该函数会初始化必要的变量，创建根菜单和其他最基本的菜单列表和菜单项。
  * 
  * @param  {void} undefined : 
  */
 void MENU_Init(void);
 
 /**
- * @brief : 
+ * @brief : 初始化各菜单列表和菜单项。
+ * 注意：该函数在外部定义，不需初始化最基本的菜单列表和菜单项。该函数需要用户手动调用。
+ * TODO: 究竟手动调用还是自动调用？或者干脆不定义这个函数，让用户自己去写？
  * 
  * @param  {void} undefined : 
  */
 void MENU_DataSetUp(void);
 
 /**
- * @brief : 
+ * @brief : 打印屏幕。
+ * 该函数是整个菜单的屏幕打印入口，将自动根据菜单的状态打印屏幕。
  * 
  * @param  {void} undefined : 
  */
 void MENU_PrintDisp(void);
 
 /**
- * @brief : 
+ * @brief : 响应按键操作。
+ * 该函数是整个菜单的按键响应入口，将自动根据菜单的状态调用按键响应函数。
+ * 该函数不会更新屏幕显示。
  * 
  * @param  {void} undefined : 
  */
 void MENU_KeyOp(void);
 
 /**
- * @brief : 
+ * @brief : 保存整个菜单到NVM。
  * 
  * @param  {int32_t} _region : 
  */
 void MENU_Data_NvmSave(int32_t _region);
 
 /**
- * @brief : 
+ * @brief : 从NVM读取整个菜单。
  * 
- * @param  {int32_t} _region : 
+ * @param  {int32_t} _region : 所选择的局部存储区。
  */
 void MENU_Data_NvmRead(int32_t _region);
 
 /**
- * @brief : 
+ * @brief : 将一个局部存储区的数据拷贝到另一个局部存储区。
  * 
- * @param  {int32_t} _srcRegion : 
- * @param  {int32_t} _dstRegion : 
+ * @param  {int32_t} _srcRegion : 源存储序号。
+ * @param  {int32_t} _dstRegion : 目的存储区序号。
  */
 void MENU_Data_NvmCopy(int32_t _srcRegion, int32_t _dstRegion);
 
 /**
- * @brief : 
+ * @brief : 读取NVM状态标志。
  * 
- * @param  {void} undefined : 
- * @return {int32_t}        : 
+ * @return {int32_t}        : 返回读取到的状态标志。
  */
 int32_t MENU_GetNvmStatus(void);
 
 /**
- * @brief : 
+ * @brief : 设置NVM状态标志。
  * 
- * @param  {int32_t} _status : 
+ * @param  {int32_t} _status : 要设置的状态标志。
  */
 void MENU_SetNvmStatus(int32_t _status);
 
