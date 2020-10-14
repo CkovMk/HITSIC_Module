dd1b208ae1657d6563b3812c948355fdc8c62822
JerrySkywalker <jerry1999_main@outlook.com>
2020-10-15 01:24:45 +0800

    Update OLED

--- d_mk66f18/drv_disp_ssd1306_port.hpp
+++ d_mk66f18/drv_disp_ssd1306_port.hpp
@@ -51,6 +51,30 @@ inline void DISP_SSD1306_delay_ms(uint32_t ms)
 
 }
 
+//内部使用用户无需调用
+inline void OLED_SCL(uint8_t data)
+{
+    if (data)
+    {
+        GPIO_PortSet(BOARD_OLED_SCL_GPIO, 1U << BOARD_OLED_SCL_GPIO_PIN);
+    }
+    else
+    {
+        GPIO_PortClear(BOARD_OLED_SCL_GPIO, 1U << BOARD_OLED_SCL_GPIO_PIN);
+    }
+}
+//内部使用用户无需调用
+inline void OLED_SDA(uint8_t data)
+{
+    if (data)
+    {
+        GPIO_PortSet(BOARD_OLED_SDA_GPIO, 1U << BOARD_OLED_SDA_GPIO_PIN);
+    }
+    else
+    {
+        GPIO_PortClear(BOARD_OLED_SDA_GPIO, 1U << BOARD_OLED_SDA_GPIO_PIN);
+    }
+}
 
 //extern dspi_master_config_t spi_m_cfg;
 //inline void DISP_SSD1306_spiInit(void)
@@ -78,15 +102,31 @@ inline void DISP_SSD1306_delay_ms(uint32_t ms)
 //}
 inline void DISP_SSD1306_spiWrite(uint8_t data)
 {
-	static dspi_transfer_t oled_spi_xfer =
-		{
-			.txData = NULL,
-			.rxData = NULL,
-			.dataSize = 1,
-			.configFlags = OLED_SPI_MasterCtarn | OLED_SPI_MasterPcsn | kDSPI_MasterPcsContinuous,
-		};
-	oled_spi_xfer.txData = &data;
-	DSPI_MasterTransferBlocking(OLED_SPI_BASE, &oled_spi_xfer);
+
+//    static dspi_transfer_t oled_spi_xfer =
+//		{
+//			.txData = NULL,
+//			.rxData = NULL,
+//			.dataSize = 1,
+//			.configFlags = OLED_SPI_MasterCtarn | OLED_SPI_MasterPcsn | kDSPI_MasterPcsContinuous,
+//		};
+//	oled_spi_xfer.txData = &data;
+//	DSPI_MasterTransferBlocking(OLED_SPI_BASE, &oled_spi_xfer);
+
+    uint8_t i=8;
+
+    DISP_SSD1306_gpioSetD_C(1);
+    OLED_SCL(0);
+    while(i--)
+    {
+        if(data&0x80){OLED_SDA(1);}
+        else{OLED_SDA(0);}
+        OLED_SCL(1);
+
+        OLED_SCL(0);
+        data<<=1;
+    }
+
 }
 
 
