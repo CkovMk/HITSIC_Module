d8f49d080b22ef20ca896376aa319a0771c22412
CkovMk <maqike@qq.com>
2020-10-17 15:58:06 +0800

    继续完善EDMADVP

--- utilities/drv_edmadvp.cpp
+++ utilities/drv_edmadvp.cpp
@@ -3,21 +3,117 @@
 
 #if defined(HITSIC_USE_EDMADVP) && (HITSIC_USE_EDMADVP > 0)
 
-void CAMERA_Init()
-{
-/*****************UART*INIT*******************/
-	const uart_config_t camera_uart_config = {
-		.baudRate_Bps = 9600,
-		.parityMode = kUART_ParityDisabled,
-		.stopBitCount = kUART_OneStopBit,
-		.txFifoWatermark = 0,
-		.rxFifoWatermark = 0,
-		.idleType = kUART_IdleTypeStartBit,
-		.enableTx = true,
-		.enableRx = true
-	};
+static edmadvp_handle_t *edmadvp_handleList[EDMADVP_CNT];
 
-	while (kStatus_Success != UART_Init(CAM_UART_BASE, &camera_uart_config, CAMERA_UART_CLOCK_SOURCE));
+status_t EDMADVP_Init(EDMADVP_Type *base, const edmadvp_config_t *config)
+{
+	assert(base);
+	assert(config);
+	/*TODO: more assert*/
+
+	base->imgSize = config->width * config->height * config->bytesPerPixel;
+
+	PORT_SetPinInterruptConfig(base->vsnc_intc, base->vsnc_pin, kPORT_InterruptOrDMADisabled);
+	
+	extInt_t::insert(base->vsnc_intc, base->vsnc_pin, __handler/*TODO: fix this*/);
+	DisableIRQ(base->dmaIrqn);
+	NVIC_SetPriority(base->dmaIrqn, base->dmaIrqPrio);
+
+	DMAMUX_SetSource(EDMADVP0_DMAMUX_INST, base->dmauxChannel, base->dmamuxRequestSrc);
+	DMAMUX_EnableChannel(EDMADVP0_DMAMUX_INST, base->dmauxChannel);//启动DMAMUX通道0，按DMAMUX_SetSource设置
+
+	EDMA_SetBandWidth(EDMADVP0_DMA_INST, base->dmaChannel, kEDMA_BandwidthStall8Cycle);//休息
+	//EDMA_SetChannelPreemptionConfig(DMA0,0u,&ecpc);//优先级很高
+	EDMA_EnableAutoStopRequest(EDMADVP0_DMA_INST, base->dmaChannel, true);
+	
+	EDMA_CreateHandle(&base->edmaHandle, EDMADVP0_DMA_INST, base->dmaChannel);//句柄存有使用那个通道的信息
+	EDMA_SetCallback(&base->edmaHandle, CAMERA_Xfer_EdmaCallback/*TODO:Fix*/, base);//回调函数设置
+	
+
+}
+
+void EDMADVP_Deinit(EDMADVP_Type *base);
+
+// void EDMADVP_EnableInterrupts(EDMADVP_Type *base, uint32_t mask);
+
+// void EDMADVP_DisableInterrupts(EDMADVP_Type *base, uint32_t mask);
+
+status_t EDMADVP_TransferCreateHandle(EDMADVP_Type *base, csi_handle_t *handle, csi_transfer_callback_t callback, void *userData)
+{
+	assert(base);
+	assert(handle);
+
+	uint32_t instance = EDMADVP_GetInstance(base);
+
+	handle->callback = callback;
+	handle->userData = userData;
+	
+	edmadvp_handleList[instance] = handle;
+
+	EDMA_PrepareTransfer(&handle->xferCfg, (void *)(base->dmaDataAddress)), 1,
+		4096/*this is incorrect, will be set to NULL later.*/, 1, 1, base->imgSize, kEDMA_PeripheralToMemory);
+	handle->xferCfg.destAddr = NULL;
+	EnableIRQ(base->dmaIrqn);
+
+}
+
+status_t EDMADVP_TransferStart(EDMADVP_Type *base, edmadvp_handle_t handle)
+{
+	if(handle->xferCfg.destAddr != NULL)
+	{
+		return kStatus_Fail;
+	}
+	if(handle->emptyBufferQueue.empty())
+	{
+		return kStatus_EDMADVP_NoEmptyBuffer;
+	}
+	handle->xferCfg.destAddr = handle->emptyBufferQueue.front();
+	handle->emptyBufferQueue.pop();
+}
+
+status_t EDMADVP_TransferStop(EDMADVP_Type *base, edmadvp_handle_t handle)
+{
+	
+}
+
+status_t EDMADVP_TransferSubmitEmptyBuffer(EDMADVP_Type *base, edmadvp_handle_t *handle, uint8_t* frameBuffer)
+{
+	handle->emptyBufferQueue.push(frameBuffer);
+}
+
+status_t EDMADVP_TransferGetFullBuffer(EDMADVP_Type *base, edmadvp_handle_t *handle, uint8_t *frameBuffer)
+{
+	frameBuffer;
+}
+
+void EDMADVP_ExtIntHandler(void)//vsnc
+{
+
+}
+
+void EDMADVP_DmaIntHandler(void)//dma finish
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void EDMADVP_Init()
+{
 
 /**************GPIO****INIT******************/
 	camera_disable_vsync_irq();
--- utilities/drv_edmadvp.hpp
+++ utilities/drv_edmadvp.hpp
@@ -32,11 +32,17 @@
 #define HITSIC_USE_EDMADVP (1U) //TODO: REMOVE this after complete
 #if defined(HITSIC_USE_EDMADVP) && (HITSIC_USE_EDMADVP > 0)
 
-#define EDMADVP_DAT0_GPIO		RTEPIN_BOARD_CAM_D0_GPIO
-#define EDMADVP_DAT0_PORT		RTEPIN_BOARD_CAM_D0_PORT
-#define EDMADVP_DAT0_PIN		RTEPIN_BOARD_CAM_D0_PIN
+#define EDMADVP_CNT (1U);
 
-#define EDMADVP_DATA_DMA_ADDR		(((uint32_t)GPIO_CAM_D0_GPIO)+0x10+GPIO_CAM_D0_PIN/8)
+#define EDMADVP0_DAT0_GPIO		RTEPIN_BOARD_CAM_D0_GPIO
+#define EDMADVP0_DAT0_PORT		RTEPIN_BOARD_CAM_D0_PORT
+#define EDMADVP0_DAT0_PIN		RTEPIN_BOARD_CAM_D0_PIN
+
+#define EDMADVP0_DMA_INST        (DMA0)
+#define EDMADVP0_DMAMUX_INST        (DMA0)
+#define EDMADVP0_DATA_DMA_ADDR		(((uint32_t)RTEPIN_BOARD_CAM_D0_GPIO)+0x10+RTEPIN_BOARD_CAM_D0_PIN/8)
+
+#define kStatusGroup_EDMADVP (200U)
 
 struct EDMADVP_Type
 {
@@ -45,25 +51,51 @@ struct EDMADVP_Type
     INTC_Type* pclk_intc;
     uint32_t pclk_pin;
 
-    uint32_t dmaAddress;
+    uint32_t dmaDataAddress;
     uint32_t dmaChannel;
     uint32_t dmauxChannel;
+    uint32_t dmamuxRequestSrc;
     uint32_t dmaIrqn;
     uint32_t dmaIrqPrio;
+
+    uint32_t interruptFlag;
+    uint32_t interruptMask;
+
+    uint32_t imgSize;
+    edma_handle_t edmaHandle;
 };
 
 EDMADVP_Type __EDMADVP0 = 
 {
-    .vsnc_intc = RTEPIN_BOARD_CAM_VSNC_PORT;
-    .vsnc_pin = RTEPIN_BOARD_CAM_VSNC_PIN;
-    .pclk_intc = RTEPIN_BOARD_CAM_PCLK_PORT;
-    .pclk_pin = RTEPIN_BOARD_CAM_PCLK_PIN;
+    .vsnc_intc = RTEPIN_BOARD_CAM_VSNC_PORT,
+    .vsnc_pin = RTEPIN_BOARD_CAM_VSNC_PIN,
+    .pclk_intc = RTEPIN_BOARD_CAM_PCLK_PORT,
+    .pclk_pin = RTEPIN_BOARD_CAM_PCLK_PIN,
 
-    .dmaAddress = EDMADVP_DATA_DMA_ADDR;
-    .dmaChannel = 0U;
-    .dmauxChannel = 0U;
-    .dmaIrqn = DMA0_DMA16_IRQn;
-    .dmaIrqPrio = 2U;
+    .dmaDataAddress = EDMADVP0_DATA_DMA_ADDR,
+    .dmaChannel = 0U,
+    .dmauxChannel = 0U,
+    .dmamuxRequestSrc = 0U;
+    .dmaIrqn = DMA0_DMA16_IRQn,
+    .dmaIrqPrio = 2U,
+
+    .interruptFlag = 0,
+    .interruptMask = 0,
+
+    .imgSize = 0U,
+    .edmaHandle = 
+    {
+        .callback = NULL,
+        .userData = NULL,
+        .base = DMA0,
+        .tcdPool = NULL,
+        .channel = 0U,
+        .header = 0U,
+        .tail = 0U,
+        .tcdUsed = 0U,
+        .tcdSize = 0U,
+        .flags = 0U,
+    },
 };
 
 static constexpr EDMADVP_Type* EDMADVP0 = &__EDMADVP0;
@@ -76,27 +108,91 @@ inline void EDMADVP_Delay_ms(uint32_t ms)
     }
 }
 
+inline uint32_t EDMADVP_GetInstance(EDMADVP_Type* base)
+{
+    switch(base)
+    {
+    case EDMADVP0:
+        return 0;
+    default:
+        assert(0);//TODO: print assert message HERE!
+    }
+}
+
 #endif // ! HITSIC_USE_EDMADVP
 /** ! port */
 
 #if defined(HITSIC_USE_EDMADVP) && (HITSIC_USE_EDMADVP > 0)
 
-#define EDMADVP_VSNC_IRQ_ENABLE() (PORT_SetPinInterruptConfig(EDMADVP_VSNC_PORT, EDMADVP_VSNC_PIN, kPORT_InterruptFallingEdge))
-#define EDMADVP_VSNC_IRQ_DISABLE() (PORT_SetPinInterruptConfig(EDMADVP_VSNC_PORT, EDMADVP_VSNC_PIN, kPORT_InterruptOrDMADisabled))
+// #define EDMADVP_VSNC_IRQ_ENABLE() (PORT_SetPinInterruptConfig(EDMADVP_VSNC_PORT, EDMADVP_VSNC_PIN, kPORT_InterruptFallingEdge))
+// #define EDMADVP_VSNC_IRQ_DISABLE() (PORT_SetPinInterruptConfig(EDMADVP_VSNC_PORT, EDMADVP_VSNC_PIN, kPORT_InterruptOrDMADisabled))
 
-#define EDMADVP_PCLK_IRQ_ENABLE()	(EnableIRQ(EDMADVP_PCLK_DMA_IRQn))
-#define EDMADVP_PCLK_IRQ_DISABLE()	(DisableIRQ(EDMADVP_PCLK_DMA_IRQn))
+// #define EDMADVP_PCLK_IRQ_ENABLE()	(EnableIRQ(EDMADVP_PCLK_DMA_IRQn))
+// #define EDMADVP_PCLK_IRQ_DISABLE()	(DisableIRQ(EDMADVP_PCLK_DMA_IRQn))
 
-struct edmadvp_xferHandle_t
+
+
+/*! @brief Error codes for the CSI driver. */
+enum
+{
+    kStatus_EDMADVP_NoEmptyBuffer = MAKE_STATUS(kStatusGroup_EDMADVP, 0), /*!< No empty frame buffer in queue to load to CSI. */
+    kStatus_EDMADVP_NoFullBuffer  = MAKE_STATUS(kStatusGroup_EDMADVP, 1), /*!< No full frame buffer in queue to read out. */
+    kStatus_EDMADVP_QueueFull = MAKE_STATUS(kStatusGroup_EDMADVP, 2), /*!< Queue is full, no room to save new empty buffer. */
+    kStatus_EDMADVP_FrameDone = MAKE_STATUS(kStatusGroup_EDMADVP, 3), /*!< New frame received and saved to queue. */
+};
+
+/*! @brief CSI signal polarity. */
+enum _edmadvp_polarity_flags    //TODO: fix this
+{
+    EDMADVP_HsyncActiveLow         = 0U,                        /*!< HSYNC is active low. */
+    EDMADVP_HsyncActiveHigh        = 1U,                        /*!< HSYNC is active high. */
+    EDMADVP_DataLatchOnRisingEdge  = 1U << 1,                   /*!< Pixel data latched at rising edge of pixel clock. */
+    EDMADVP_DataLatchOnFallingEdge = 0U,                        /*!< Pixel data latched at falling edge of pixel clock. */
+    EDMADVP_VsyncActiveHigh        = 0U,                        /*!< VSYNC is active high. */
+    EDMADVP_VsyncActiveLow         = 1U << 2,                   /*!< VSYNC is active low. */
+};
+
+struct edmadvp_config_t
+{
+    uint16_t width;           /*!< Pixels of the input frame. */
+    uint16_t height;          /*!< Lines of the input frame.  */
+    uint8_t bytesPerPixel;    /*!< Bytes per pixel            */
+    uint16_t linePitch_Bytes; /*!< Frame buffer line pitch, must be 8-byte aligned. */
+    uint32_t polarityFlags;   /*!< Timing signal polarity flags, OR'ed value of @ref _csi_polarity_flags. */
+};
+
+struct edmadvp_handle_t;
+
+typedef void (*edmadvp_transfer_callback_t)(EDMADVP_Type *base, edmadvp_handle_t *handle, status_t status, void *userData);
+
+struct edmadvp_handle_t
 {//TODO: update this according to fsl_csi.h
-    uint16_t img_row, img_col;
+    
+    std::queue<uint8_t*> emptyBufferQueue;
+    edma_transfer_config_t xferCfg;
 }
 
-status_t EDMADVP_Init(EDMADVP_Type* _inst);
-status_t EDMADVP_PrepareXfer(edmadvp_xferHandle_t* _handle, uint32_t _bufferCnt, callback_t _callback);
-status_t EDMADVP_BeginXfer(EDMADVP_Type* _inst, edmadvp_xferHandle_t _handle);
-void EDMADVP_VSNC_ExtIntHandler(EDMADVP_Type* _inst);
-void EDMADVP_EdmaXferCallback(edma_handle_t * handle, void * param, bool transferDone, uint32_t tcds);
+status_t EDMADVP_Init(EDMADVP_Type *base, const edmadvp_config_t *config);
+
+void EDMADVP_Deinit(EDMADVP_Type *base);
+
+// void EDMADVP_EnableInterrupts(EDMADVP_Type *base, uint32_t mask);
+
+// void EDMADVP_DisableInterrupts(EDMADVP_Type *base, uint32_t mask);
+
+status_t EDMADVP_TransferCreateHandle(EDMADVP_Type *base, csi_handle_t *handle, csi_transfer_callback_t callback, void *userData);
+
+status_t EDMADVP_TransferStart(EDMADVP_Type *_inst, edmadvp_handle_t _handle);
+
+status_t EDMADVP_TransferStop(EDMADVP_Type *_inst, edmadvp_handle_t _handle);
+
+status_t EDMADVP_TransferSubmitEmptyBuffer(EDMADVP_Type *base, edmadvp_handle_t *handle, uint32_t frameBuffer);
+
+status_t EDMADVP_TransferGetFullBuffer(EDMADVP_Type *base, edmadvp_handle_t *handle, uint32_t *frameBuffer);
+
+void EDMADVP_VsncExtIntHandler(EDMADVP_Type* _inst);
+
+//void EDMADVP_EdmaXferCallback(edma_handle_t * handle, void * param, bool transferDone, uint32_t tcds);
 
 
 #endif // ! HITSIC_USE_EDMADVP
