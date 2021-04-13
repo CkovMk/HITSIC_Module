dbdcea7346e8ebbf3031245ad59608d83d0feac3
Chekhov Mark <maqike@qq.com>
2021-04-13 12:10:52 +0800

    更新invimu，更新示例代码

--- utilities_test/drv_invimu_test.h
+++ utilities_test/drv_invimu_test.h
@@ -1,7 +1,24 @@
 ﻿#include"drv_imu_invensense.h"
+
+#if (defined(INV_USE_HITSIC_SYSLOG) && (INV_USE_HITSIC_SYSLOG > 0))
+
+#define SYSLOG_LVL (INVIMU_LOG_LVL)
+#define SYSLOG_TAG "INVIMU"
+#include<inc_syslog.h>
+
+#else // INV_USE_HITSIC_SYSLOG
+
+#define SYSLOG_A(...) INV_PRINTF("\r\n");
+#define SYSLOG_E(...) INV_PRINTF("\r\n");
+#define SYSLOG_W(...) INV_PRINTF("\r\n");
+#define SYSLOG_I(...) INV_PRINTF("\r\n");
+#define SYSLOG_D(...) INV_PRINTF("\r\n");
+#define SYSLOG_V(...) INV_PRINTF("\r\n");
+
+#endif // ! INV_USE_HITSIC_SYSLOG
 int INV_IMU_Example(int argn, char **argv)
 {
-    inv_i2c i2c0 =
+    inv_i2c_t i2c0 =
     {
         .masterTransferBlocking = IMU_INV_I2C_TransferBlocking,
         .masterTransferNonBlocking = NULL,
@@ -10,14 +27,14 @@ int INV_IMU_Example(int argn, char **argv)
     //or
     //i2c0 spi0;
     //spi0 = .....
-    inv_imu_handle imu0 = IMU_AutoConstructI2C(i2c0, IMU_SlaveAddressAutoDetect);
+    inv_imu_handle_t imu0 = IMU_AutoConstructI2C(i2c0, IMU_SlaveAddressAutoDetect);
     //or inv_imu_handle imu0 = IMU_AutoConstructSPI(spi0);
     if (imu0 != NULL)
     {
-        INV_INFO("当前使用的传感器为 %s", IMU_Report(imu0));
+        SYSLOG_I("当前使用的传感器为 %s", IMU_Report(imu0));
         if (0 == IMU_Init(imu0, IMU_ConfigDefault()))
         {
-            INV_INFO("ST = %d,0为自检通过,自检时保持静止", IMU_SelfTest(imu0));
+            SYSLOG_I("ST = %d,0为自检通过,自检时保持静止", IMU_SelfTest(imu0));
             // Delay(10)//delay 10ms
             int loop = 10;
             while (loop--)
@@ -28,23 +45,23 @@ int INV_IMU_Example(int argn, char **argv)
                 float *buf = data;
                 IMU_Convert(imu0, data);
                 IMU_ConvertTemp(imu0, &temp);
-                printf("temp = %f", temp);
-                printf("accel(xyz) = %.3f %.3f %.3f\t gyro(xyz) = %.3f %.3f %.3f\t mag(xyz) = %.3f %.3f %.3f \r\n",
+                SYSLOG_I("temp = %f", temp);
+                SYSLOG_I("accel(xyz) = %.3f %.3f %.3f\t gyro(xyz) = %.3f %.3f %.3f\t mag(xyz) = %.3f %.3f %.3f \r\n",
                        *buf++, *buf++, *buf++, *buf++, *buf++, *buf++, *buf++, *buf++, *buf++);
             }
         }
         else
         {
-            INV_INFO("初始化失败");
+            SYSLOG_I("初始化失败");
         }
     }
     else
     {
-        INV_INFO("imu0 == NULL，没接或者iic/spi读写出错");
+        SYSLOG_I("imu0 == NULL，没接或者iic/spi读写出错");
     }
     if (imu0 != NULL)
     {
-        INV_INFO("释放资源");
+        SYSLOG_I("释放资源");
         IMU_Destruct(imu0);
     }
     return 0;
@@ -53,19 +70,19 @@ int INV_IMU_Example(int argn, char **argv)
 #include"drv_imu_inv_mpu6050.h"
 int INV_MPU6050_Example(int argn, char **argv)
 {
-    inv_i2c i2c0;
+    inv_i2c_t i2c0;
     //i2c0 = ....
     //or
     //i2c0 spi0;
     //spi0 = .....
-    inv_mpu6050_handle imu0 = MPU6050_ConstructI2C(i2c0, IMU_SlaveAddressAutoDetect);
+    inv_mpu6050_handle_t imu0 = MPU6050_ConstructI2C(i2c0, IMU_SlaveAddressAutoDetect);
     //or inv_mpu9250_handle imu0 = MPU9250_ConstructSPI(spi0);
     if (imu0 != NULL)
     {
-        INV_INFO("当前使用的传感器为 %s", MPU6050_Report(imu0));
+        SYSLOG_I("当前使用的传感器为 %s", MPU6050_Report(imu0));
         if (0 == MPU6050_Init(imu0, IMU_ConfigDefault()))
         {
-            INV_INFO("ST = %d,0为自检通过,自检时保持静止", MPU6050_SelfTest(imu0));
+            SYSLOG_I("ST = %d,0为自检通过,自检时保持静止", MPU6050_SelfTest(imu0));
             // Delay(10)//delay 10ms
             int loop = 10;
             while (loop--) {
@@ -75,24 +92,33 @@ int INV_MPU6050_Example(int argn, char **argv)
                 float *buf = data;
                 MPU6050_Convert(imu0, data);
                 MPU6050_ConvertTemp(imu0, &temp);
-                printf("temp = %f", temp);
-                printf("accel(xyz) = %.3f %.3f %.3f\t gyro(xyz) = %.3f %.3f %.3f\t mag(xyz) = %.3f %.3f %.3f",
+                SYSLOG_I("temp = %f", temp);
+                SYSLOG_I("accel(xyz) = %.3f %.3f %.3f\t gyro(xyz) = %.3f %.3f %.3f\t mag(xyz) = %.3f %.3f %.3f",
                        *buf++, *buf++, *buf++, *buf++, *buf++, *buf++, *buf++, *buf++, *buf++);
             }
         }
         else
         {
-            INV_INFO("初始化失败");
+            SYSLOG_I("初始化失败");
         }
     }
     else
     {
-        INV_INFO("imu0 == NULL，没接或者iic/spi读写出错");
+        SYSLOG_I("imu0 == NULL，没接或者iic/spi读写出错");
     }
     if (imu0 != NULL)
     {
-        INV_INFO("释放资源");
+        SYSLOG_I("释放资源");
         MPU6050_Destruct(imu0);
     }
     return 0;
 }
+
+#undef SYSLOG_LVL
+#undef SYSLOG_TAG
+#undef SYSLOG_A
+#undef SYSLOG_E
+#undef SYSLOG_W
+#undef SYSLOG_I
+#undef SYSLOG_D
+#undef SYSLOG_V
