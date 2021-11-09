bfe929e32990a3c6321a1a443f9b6f0ba2325123
王沛 <1547963558@qq.com>
2021-11-09 21:46:09 +0800

    rmcall与上位机联调成功，且成功握手。

--- d_mk24f12/sys_rmcall_port.c
+++ d_mk24f12/sys_rmcall_port.c
@@ -9,6 +9,7 @@
 #include <inc_syslog.h>
 
 extern rmcall_t rmcall_host;
+extern ECG_Data_Packet_t config_info;
 
 uart_handle_t UART_BLE_handle;
 uart_transfer_t UART_BLE_rxTransfer = {
@@ -20,9 +21,58 @@ uart_transfer_t UART_BLE_txTransfer = {
   .dataSize = 0,
 };
 
+rmcall_teleport_t teleport_host =
+{
+	RMCALL_HOST_Tx,
+	RMCALL_HOST_Rx,
+	RMCALL_HOST_TxAbort,
+	RMCALL_HOST_RxAbort,
+};
+const rmcall_config_t config_host = 	{
+		.teleport = &teleport_host
+};
+
+void ECG_recevieDataPacke(void *_recvData, uint16_t _recvSize, void *_userData)
+{
+    assert(_recvSize == 11);
+    config_info.waveform = *(uint8_t *)_recvData;
+    config_info.func     = *((uint8_t *)_recvData + 1);
+    config_info.lead     = *((uint8_t *)_recvData + 2);
+    config_info.voltage  = *((float *)(((uint8_t *)_recvData) + 3));
+    config_info.period   = *((float *)(((uint8_t *)_recvData) + 7));
+    static uint32_t retstatus = 0;
+    RMCALL_CommandSend(&rmcall_host, GEN_STATUS_ID,&retstatus, sizeof(retstatus));
+}
+
+rmcall_handle_t ECG_rDPhandle =
+{
+    .handleId = ECG_INFO_ID,
+    .handler = ECG_recevieDataPacke,
+    .userData = NULL,
+};
+
+/* 通用命令 - 握手命令 */
+void ECG_HandShake(void *_recvData, uint16_t _recvSize, void *_userData)
+{
+    assert(0U == _recvSize);
+    static uint32_t retStatus = GEN_HSHAKE_ID;
+    RMCALL_CommandSend(&rmcall_host, GEN_HSHAKE_ID, &retStatus, 4U);
+    SYSLOG_I("ECG_HandShake.");
+}
+rmcall_handle_t ECG_handShakehandle =
+{
+    .handleId = GEN_HSHAKE_ID,
+    .handler = ECG_HandShake,
+    .userData = NULL,
+};
+
 void RMCALL_PORT_Init(void)
 {
-	  UART_TransferCreateHandle(HITSIC_RMCALL_UART, &UART_BLE_handle, RMCALL_HOST_CpltCallback, NULL);
+	UART_TransferCreateHandle(HITSIC_RMCALL_UART, &UART_BLE_handle, RMCALL_HOST_CpltCallback, NULL);
+	RMCALL_Init(&rmcall_host, &config_host);
+    RMCALL_HandleInsert(&rmcall_host, &ECG_rDPhandle);
+    RMCALL_HandleInsert(&rmcall_host, &ECG_handShakehandle);
+    RMCALL_CommandRecvEnable(&rmcall_host);
 }
 
 status_t RMCALL_HOST_Tx(void *_data, uint32_t _dataSize)
--- d_mk24f12/sys_rmcall_port.h
+++ d_mk24f12/sys_rmcall_port.h
@@ -6,18 +6,33 @@
 #if defined(HITSIC_USE_RMCALL) && (HITSIC_USE_RMCALL > 0)
 
 
-#define HITSIC_RMCALL_UART UART1
+#define HITSIC_RMCALL_UART UART3
 #define HITSIC_RMCALL_CLKSRC   UART1_CLK_SRC
-#define HITSIC_RMCALL_UART_CLK_FREQ CLOCK_GetFreq(UART1_CLK_SRC)
+#define HITSIC_RMCALL_UART_CLK_FREQ CLOCK_GetFreq(UART3_CLK_SRC)
 
 extern uart_handle_t UART_BLE_handle;
 extern uart_transfer_t UART_BLE_rxTransfer;
 extern uart_transfer_t UART_BLE_txTransfer;
 
 #define HITSIC_RMCALL_HEADER_MAGIC 0x554768A0U
-#define RMCALL_SYSLOG_LVL (2U)
+#define RMCALL_SYSLOG_LVL (3U)
 #define HITSIC_RMCALL_PUBLIC_BUF_SIZE (256U)
 #define kStatusGroup_RMCALL (201U)
+
+//数据包定义
+__PACKED struct _ECG_Data_Packet_t
+{
+	uint32_t waveform;
+	uint32_t func;
+	uint32_t lead;
+	float voltage;
+	float period;
+};
+typedef struct _ECG_Data_Packet_t ECG_Data_Packet_t;
+//指令宏
+#define GEN_STATUS_ID    0xaaaa
+#define GEN_HSHAKE_ID	 0x5555
+#define ECG_INFO_ID      0xabcd
 
 
 status_t RMCALL_HOST_Tx(void *_data, uint32_t _dataSize);
